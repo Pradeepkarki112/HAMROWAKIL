04a763d408c99f7c0f8b02c94638a0662b690c79
Pradeepkarki112 <pradeepkarki112@gmail.com>
2023-03-09 15:06:28 +0545

    fixes

--- src/website/Aboutus.js
+++ src/website/Aboutus.js
@@ -1,7 +1,6 @@
 // import React from 'react';
 // import Navbar from './Navbar';
 // import Footer from './Footer';
-// // import { FaQuoteLeft } from 'react-icons/fa';
 
 // const Aboutus = () => {
 //   return (
@@ -19,7 +18,6 @@
 //               As a founder of Hamrowakil, I am proud to have been a part of creating a platform that bridges the gap between lawyers and clients. Our goal has always been to provide a seamless experience for both parties, and we have achieved this through the use of modern technology and innovative features.
 //               </p>
 //               <div className="flex items-center">
-//                 {/* <FaQuoteLeft size={20} className="text-gray-400 mr-2" /> */}
 //                 <p className="text-sm font-medium text-gray-600">We believe in pushing boundaries and exceeding expectations.</p>
 //               </div>
 //             </div>
@@ -35,7 +33,6 @@
 //               It has been an incredible journey building Hamrowakil alongside my fellow founders. Our shared vision of making legal services more accessible and convenient for everyone has been the driving force behind this project. I am excited to see where this platform takes us in the future.
 //               </p>
 //               <div className="flex items-center">
-//                 {/* <FaQuoteLeft size={20} className="text-gray-400 mr-2" /> */}
 //                 <p className="text-sm font-medium text-gray-600">What we do is more than just a job. It's a passion.</p>
 //               </div>
 //             </div>
@@ -51,7 +48,6 @@
 //               Hamrowakil is more than just a legal services platform to me, it is a means to empower individuals by giving them access to quality legal counsel. I look forward to seeing the positive impact that Hamrowakil has on our clients and the legal industry as a whole.
 //               </p>
 //               <div className="flex items-center">
-//                 {/* <FaQuoteLeft size={20} className="text-gray-400 mr-2" /> */}
 //                 <p className="text-sm font-medium text-gray-600">We are dedicated to delivering the best customer experience possible.</p>
 //               </div>
 //             </div>
@@ -66,7 +62,6 @@
 
 // export default Aboutus;
 
-
 import * as React from "react";
 import {
   Card,
@@ -79,8 +74,8 @@ import {
 } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { card, message, testimonialsTitle } from "../home/styles";
-import Navbar from './Navbar';
-import Footer from './Footer';
+import Navbar from "./Navbar";
+import Footer from "./Footer";
 
 const theme = createTheme();
 
@@ -88,7 +83,72 @@ const Aboutus = () => {
   return (
     <ThemeProvider theme={theme}>
       <CssBaseline />
-      <div className='bg-[#000300]'><Navbar /></div>
+      <div className="bg-[#000300]">
+        <Navbar />
+      </div>
+      <div class="flex items-center justify-center pt-16">
+        <div class="w-1/12 lg:w-1/2 flex items-center justify-center pt-24">
+          <img
+            src='/images/Client__Signup.jpeg'
+            alt="Hamrowakil Logo"
+            className="w-auto object-contain mx-auto"
+          />
+        </div>
+        <div class="w-full lg:w-1/2 p-10 text-justify">
+          <h2 class="text-3xl font-bold mb-5">About Hamrowakil</h2>
+          <p class="text-lg mb-5">
+            Welcome to Hamrowakil, the online platform that connects clients
+            with verified lawyers in Nepal. Our mission is to make legal
+            services more accessible and convenient for everyone, regardless of
+            their location or financial situation. We understand that legal
+            issues can be stressful and complicated, and we want to provide a
+            solution that makes it easy for you to find a trusted lawyer who can
+            help you with your specific needs.
+          </p>
+          <p class="text-lg mb-5">
+            At Hamrowakil, we take pride in
+            our commitment to providing high-quality legal services to our
+            clients. Our team of experienced lawyers is dedicated to delivering
+            personalized and timely solutions to our clients' legal issues. We
+            believe that every client deserves the best legal representation,
+            and we work tirelessly to ensure that our services meet and exceed
+            their expectations.
+          </p>
+        </div>
+      </div>
+      <div class="flex items-center justify-center pt-12">
+        <div class="w-full lg:w-1/2 p-10 text-justify">
+          <p class="text-lg mb-5">
+            Our platform is designed with the user in mind, making it easy to
+            find the right lawyer for your needs. With our easy-to-use
+            interface, you can browse through our list of verified lawyers, read
+            reviews and ratings from previous clients, and connect with a lawyer
+            who best fits your needs. We also offer a range of legal services,
+            including civil litigation, insurance defense, personal injury, and
+            case investigation.
+          </p>
+          <p class="text-lg mb-5">
+            At Hamrowakil, we are committed to upholding the
+            highest standards of professionalism and ethics in the legal
+            profession. Our team of lawyers undergoes a rigorous verification
+            process to ensure that they meet our strict requirements for
+            expertise and integrity. We also offer ongoing training and support
+            to our lawyers to ensure that they stay up-to-date with the latest
+            legal developments and best practices. We are proud to be a part of
+            the growing legal tech industry in Nepal and are excited to continue
+            innovating and improving our platform to better serve our clients.
+            Thank you for choosing Hamrowakil, and we look forward to helping
+            you with your legal needs.
+          </p>
+        </div>
+        <div class="lg:w-1/2 flex items-center justify-center">
+          <img
+            src="/images/Client__Signin.jpeg"
+            alt="Hamrowakil Logo"
+            className="w-full h-auto"
+          />
+        </div>
+      </div>
       <main>
         <Container sx={{ py: 8 }} maxWidth="md">
           <Typography variant="h3" sx={testimonialsTitle}>
@@ -109,7 +169,8 @@ const Aboutus = () => {
                     component="h2"
                     sx={message}
                   >
-                    "We believe in pushing boundaries and exceeding expectations."
+                    "We believe in pushing boundaries and exceeding
+                    expectations."
                   </Typography>
                   <Typography variant="h6">- Manish</Typography>
                 </CardContent>
@@ -130,7 +191,8 @@ const Aboutus = () => {
                     component="h2"
                     sx={message}
                   >
-                    "We believe in pushing boundaries and exceeding expectations."
+                    "We believe in pushing boundaries and exceeding
+                    expectations."
                   </Typography>
                   <Typography variant="h6">- Pradeep</Typography>
                 </CardContent>
@@ -160,7 +222,9 @@ const Aboutus = () => {
           </Grid>
         </Container>
       </main>
-      <div className='bg-[#000300]'><Footer /></div>
+      <div className="bg-[#000300]">
+        <Footer />
+      </div>
     </ThemeProvider>
   );
 };
